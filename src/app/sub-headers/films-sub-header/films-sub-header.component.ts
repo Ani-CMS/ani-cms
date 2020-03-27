0fb683026018b04a40bb4b6f17bbc9ca3cbcbc49
Martin Jaskulla <martin.jaskulla@accenture.com>
2020-03-27 07:24:04 +0100

    Add films and works sub header components

---
+++ src/app/sub-headers/films-sub-header/films-sub-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-films-sub-header',
+  templateUrl: './films-sub-header.component.html',
+  styleUrls: ['./films-sub-header.component.css']
+})
+export class FilmsSubHeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sub-headers/works-sub-header/works-sub-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-works-sub-header',
+  templateUrl: './works-sub-header.component.html',
+  styleUrls: ['./works-sub-header.component.css']
+})
+export class WorksSubHeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
