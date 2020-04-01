310a35447efa6349869d10b1f121b63b6877da56
Martin Jaskulla <martin.jaskulla@accenture.com>
2020-04-01 17:58:37 +0200

    Move todos to README.md

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # TODO
 
-Try adding prettier to netlify pipeline
+
+  Mobile
+    - Check on real phone
+  Subheader:
+    - Jeder subheader gleiches Pattern, stylen mit nth-child -> Titles austauschen.
+    - Multiple links on one row
+    - One single Subheader component with inputs, used on home, films, works
+    - on home the inputs can be sorted alphabetically
+    - id of content used in url
+   Other
+    - Turn these $ functions, otherwise all requests are made on app startup
+    - PWA
+    - upcoming margin between events needs to be increased -> Challenge Dani
+
--- src/app/contentful.service.ts
+++ src/app/contentful.service.ts
@@ -13,24 +13,6 @@ import { NewComponent, RichTextConfig } from './rich-text/rich-text.component'
 import { UpcomingEvent } from './pages/upcoming/upcoming-event/upcoming-event.component'
 import { LongText } from './pages/texts/texts.component'
 
-/*
- TODO
-  Mobile
-    - Check on real phone
-  Subheader:
-    - Jeder subheader gleiches Pattern, stylen mit nth-child -> Titles austauschen.
-    - Multiple links on one row
-    - One single Subheader component with inputs, used on home, films, works
-    - on home the inputs can be sorted alphabetically
-    - id of content used in url
-   Other
-    - on text page h1 full width in rich text
-    - Turn these $ functions, otherwise all requests are made on app startup
-    - PWA
-
-    - upcoming margin between events needs to be increased -> Challenge Dani
-     */
-
 @Injectable({
   providedIn: 'root',
 })
