c9a8bb1f669a70498b23d847b5a194d49655f7bf
Martin Jaskulla <martin.jaskulla@accenture.com>
2020-05-01 15:05:12 +0200

    test: Add test scripts to package.json, add a failing test to test GitHub actions ci pipeline, configure Karma to be able to use a headless Chrome

---
+++ src/app/rich-text-parser.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing'
+
+import { RichTextParserService } from './rich-text-parser.service'
+
+describe('RichTextParserService', () => {
+  let service: RichTextParserService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(RichTextParserService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeFalsy()
+  })
+})
